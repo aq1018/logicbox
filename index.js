ccfddda99b7bea6dd57d77b76076cbbe35f18c3f
Aaron Qian <aq1018@gmail.com>
2014-01-23 14:05:25 -0800

    umdify package.

--- index.js
+++ index.js
@@ -1,79 +1,94 @@
-'use strict';
+(function (root, factory) {
+    'use strict';
 
-var _       = require('underscore');
-var async   = require('async');
-
-var DEFAULT_OPTIONS = {
-    basePath: process.cwd()
-};
-
-module.exports = function(env, options) {
-    var actions;
-
-    function _require(file) {
-        file = require('path').resolve(options.basePath, file);
-        return require(file);
+    if (typeof exports === 'object') {
+        // CommonJS
+        var _           = require('underscore');
+        var async       = require('async');
+        module.exports  = factory(_, async);
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['underscore', 'async'], factory);
+    } else {
+        // Global Variables
+        root.logicbox = factory(root._, root.async);
     }
+}(this, function (_, async) {
+    'use strict';
 
-    function coerce(thing) {
-        if('function' === typeof thing) { return thing; }
-        if('string' === typeof thing) { return _require(thing); }
+    var DEFAULT_OPTIONS = {
+        basePath: process.cwd()
+    };
 
-        throw new TypeError('only functions or strings are allowed.');
-    }
+    return function(env, options) {
+        var actions;
 
-    function array() {
-        return _.chain(arguments).flatten().compact().value();
-    }
-
-    function normalize(action) {
-        if(typeof action === 'string' || typeof action === 'function') {
-            action = { handler: action };
+        function _require(file) {
+            file = require('path').resolve(options.basePath, file);
+            return require(file);
         }
 
-        _.each(['observer', 'pre', 'post'], function(key) {
-            action[key] = _.map(array(action[key]), coerce);
-        });
+        function coerce(thing) {
+            if('function' === typeof thing) { return thing; }
+            if('string' === typeof thing) { return _require(thing); }
 
-        action.handler = coerce(action.handler);
+            throw new TypeError('only functions or strings are allowed.');
+        }
 
-        return action;
-    }
+        function array() {
+            return _.chain(arguments).flatten().compact().value();
+        }
 
-    function compose(action) {
-        var chain, observers;
+        function normalize(action) {
+            if(typeof action === 'string' || typeof action === 'function') {
+                action = { handler: action };
+            }
 
-        action      = normalize(action);
-        chain       = array(action.pre, action.handler, action.post);
-        observers   = array(action.observer);
-
-        var iterator = function(input, fn, cb) { fn(env, input, cb); };
-
-        return function(input, cb) {
-            async.reduce(chain, input, iterator, function(err, output) {
-                observers.forEach(function(observer) {
-                    observer(env, output, err);
-                });
-                cb(err, output);
+            _.each(['observer', 'pre', 'post'], function(key) {
+                action[key] = _.map(array(action[key]), coerce);
             });
 
-        };
-    }
+            action.handler = coerce(action.handler);
 
-    options = _.extend({}, DEFAULT_OPTIONS, options);
-
-    actions = _.reduce(options.actions, function(memo, action, name) {
-        memo[name] = compose(action);
-        return memo;
-    }, {});
-
-    return function dispatch(name, input, cb) {
-        var handler = actions[name];
-
-        if(!handler) {
-            throw new Error('dispatcher cannot find action named "' + name + '"');
+            return action;
         }
 
-        handler(input, cb);
+        function compose(action) {
+            var chain, observers;
+
+            action      = normalize(action);
+            chain       = array(action.pre, action.handler, action.post);
+            observers   = array(action.observer);
+
+            var iterator = function(input, fn, cb) { fn(env, input, cb); };
+
+            return function(input, cb) {
+                async.reduce(chain, input, iterator, function(err, output) {
+                    observers.forEach(function(observer) {
+                        observer(env, output, err);
+                    });
+                    cb(err, output);
+                });
+
+            };
+        }
+
+        options = _.extend({}, DEFAULT_OPTIONS, options);
+
+        actions = _.reduce(options.actions, function(memo, action, name) {
+            memo[name] = compose(action);
+            return memo;
+        }, {});
+
+        return function dispatch(name, input, cb) {
+            var handler = actions[name];
+
+            if(!handler) {
+                throw new Error('dispatcher cannot find action named "' + name + '"');
+            }
+
+            handler(input, cb);
+        };
     };
-};
+
+}));
